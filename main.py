b64e95d63346e21a6e446efe51611fdf5013ba5b
fay wu <wru.fay@gmail.com>
2026-05-21 19:07:41 -0300

    add CORS config for deployment

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 # Command to run the backend server locally: uvicorn main:app --reload
 
 # This file is used to initialize FastAPI, register routes and configure middleware*
 
 app = FastAPI()
+
+# CORS configuration for deployment
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def root():
